ab46dae5ebf74815ff79b0bc7730b5792ff3a716
Meharverma <78215373+Meharverma@users.noreply.github.com>
2021-02-26 18:52:30 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,22 @@
+class Chain{
+    constructor(bodyA,bodyB){
+        var Option = {
+            bodyA:bodyA,
+            bodyB:bodyB,
+            length:10,
+            stiffness:0.04,
+        
+        }    
+        this.chain = Constraint.create(Option);
+World.add(world,this.chain);
+    }
+    display(){
+        strokeWeight(3);
+        line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y);
+    
+
+
+
+
+    }
+}
